f9ef23d44d8e2c93f9082b15c9f7c6184ebd2f86
XOF <matias.pizarro.o96@gmail.com>
2021-05-25 00:07:28 -0400

    little introduction

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '/components/layout'
+import Layout, {siteTitle} from '/components/layout'
 import utilStyles from '/styles/utils.module.css'
 import styles from '/styles/Home.module.css'
 import Link from "next/link";
@@ -12,7 +12,11 @@ export default function Home() {
                 <title>{siteTitle}</title>
             </Head>
             <section className={utilStyles.headingMd}>
-                <p>[Your Self Introduction]</p>
+                <p>
+                    Hi everyone, my name is Matías and I'm a computer science student.
+                    I love languages, especially french. I made this blog to teach you some spanish and practice my french.
+                    Alors, c'est parti !
+                </p>
                 <p>
                     (This is a sample website - you’ll be building a site like this on{' '}
                     <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
