2633373a2813f9ecd911444e7fd7624764c16994
XOF <matias.pizarro.o96@gmail.com>
2021-05-30 23:15:22 -0400

    Navbar implemented using Tailwindcss

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '/styles/globals.css'
+// import 'tailwindcss/tailwind.css'  // To use tailwind directly
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,13 @@
 import Image from 'next/image';
-import Layout from '/components/layout';
-import utilStyles from '/styles/utils.module.css'
-import {getSortedPostsData} from '/lib/posts'
-
 import Link from 'next/link'
+
+import Layout from '/components/layout';
+import Navbar from '/components/navbar'
 import Date from '/components/date'
+
+import utilStyles from '/styles/utils.module.css'
+
+import {getSortedPostsData} from '/lib/posts'
 
 const ProfileImage = () => (
     <Image
@@ -27,25 +30,28 @@ export async function getStaticProps() {
 export default function About({allPostsData}) {
 
     return (
-        <Layout>
-            <h1>This is about me :)</h1>
-            <ProfileImage/>
-            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-                <h2 className={utilStyles.headingLg}>Blog</h2>
-                <ul className={utilStyles.list}>
-                    {allPostsData.map(({id, date, title}) => (
-                        <li className={utilStyles.listItem} key={id}>
-                            <Link href={`/posts/${id}`}>
-                                <a>{title}</a>
-                            </Link>
-                            <br />
-                            <small className={utilStyles.lightText}>
-                                <Date dateString={date} />
-                            </small>
-                        </li>
-                    ))}
-                </ul>
-            </section>
-        </Layout>
+        <div>
+            <Navbar/>
+            <Layout>
+                <h1>This is about me :)</h1>
+                <ProfileImage/>
+                <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                    <h2 className={utilStyles.headingLg}>Blog</h2>
+                    <ul className={utilStyles.list}>
+                        {allPostsData.map(({id, date, title}) => (
+                            <li className={utilStyles.listItem} key={id}>
+                                <Link href={`/posts/${id}`}>
+                                    <a>{title}</a>
+                                </Link>
+                                <br/>
+                                <small className={utilStyles.lightText}>
+                                    <Date dateString={date}/>
+                                </small>
+                            </li>
+                        ))}
+                    </ul>
+                </section>
+            </Layout>
+        </div>
     )
 }
